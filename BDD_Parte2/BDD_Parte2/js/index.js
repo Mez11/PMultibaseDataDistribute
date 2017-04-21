a1a2beea10467fda5907d9c0f7810347c0af849b
charlis397 <charlis397@hotmail.com>
2017-04-21 01:08:33 -0500

    Desconozco como se tenia que subir, pero esta es la parte que nos toco... att: Luis y Carlos

---
+++ BDD_Parte2/BDD_Parte2/js/index.js
@@ -0,0 +1,17 @@
+  function primero()
+        {
+           $('#myModal').modal('show')
+            
+        }
+
+
+
+
+
+  $(document).ready(function()
+    {
+        $("#Musica").on("click",primero);
+
+
+
+    });
